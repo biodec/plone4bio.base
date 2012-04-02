e5affb5ee134e5fac9d57883c5045d1dc298a431
Mauro Amico <mauro@biodec.com>
2012-04-03 00:59:15 +0200

    pep8

--- src/plone4bio/base/Extensions/Install.py
+++ src/plone4bio/base/Extensions/Install.py
@@ -2,24 +2,25 @@ import transaction
 from Products.CMFCore.utils import getToolByName
 
 PRODUCT_DEPENDENCIES = ()
-                        
+
 EXTENSION_PROFILES = ('plone4bio.base:default',)
+
 
 def install(self, reinstall=False):
     """Install a set of products (which themselves may either use Install.py
     or GenericSetup extension profiles for their configuration) and then
     install a set of extension profiles.
-    
+
     One of the extension profiles we install is that of this product. This
     works because an Install.py installation script (such as this one) takes
-    precedence over extension profiles for the same product in 
-    portal_quickinstaller. 
-    
+    precedence over extension profiles for the same product in
+    portal_quickinstaller.
+
     We do this because it is not possible to install other products during
     the execution of an extension profile (i.e. we cannot do this during
     the importVarious step for this profile).
     """
-    
+
     portal_quickinstaller = getToolByName(self, 'portal_quickinstaller')
     portal_setup = getToolByName(self, 'portal_setup')
 
@@ -30,9 +31,11 @@ def install(self, reinstall=False):
         elif not portal_quickinstaller.isProductInstalled(product):
             portal_quickinstaller.installProduct(product)
             transaction.savepoint()
-    
+
     for extension_id in EXTENSION_PROFILES:
-        portal_setup.runAllImportStepsFromProfile('profile-%s' % extension_id, purge_old=False)
+        portal_setup.runAllImportStepsFromProfile('profile-%s' % extension_id,
+                                                              purge_old=False)
         product_name = extension_id.split(':')[0]
         portal_quickinstaller.notifyInstalled(product_name)
         transaction.savepoint()
+
